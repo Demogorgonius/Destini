644439903ae9032782452ced3d2360f8a0b0c6e8
Zvontsov Sergey <zvoncov@gmail.com>
2023-02-02 12:11:07 +0300

    Complete UI (Add subviews and make constraints )

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -45,6 +45,7 @@ class ViewController: UIViewController {
         label.font = labels.labelFont
         label.textColor = labels.labelTextColor
         label.numberOfLines = 0
+        label.textAlignment = .center
         label.adjustsFontSizeToFitWidth = true
         label.minimumScaleFactor = 0.5
         label.text = "Question text."
@@ -71,6 +72,8 @@ class ViewController: UIViewController {
     
     lazy var backgroundImageView: UIImageView = {
         let image = UIImageView(image: #imageLiteral(resourceName: "background"))
+        image.sizeToFit()
+        
         return image
     }()
     
@@ -87,7 +90,37 @@ class ViewController: UIViewController {
         view.addSubview(backgroundImageView)
         view.addSubview(verticalStackView)
         
+        backgroundImageView.snp.makeConstraints { (make) in
+            make.height.equalToSuperview()
+            make.width.equalToSuperview().offset(5)
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(-1)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
         
+        verticalStackView.snp.makeConstraints { (make) in
+            make.height.equalToSuperview().offset(-40)
+            make.width.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview().offset(0)
+            make.bottom.equalToSuperview()
+        }
+        
+        firstButton.snp.makeConstraints { make in
+            make.height.equalTo(80)
+            make.trailing.equalToSuperview().offset(-15)
+            make.leading.equalToSuperview().offset(15)
+            make.centerX.equalTo(verticalStackView)
+        }
+        
+        secondButton.snp.makeConstraints { make in
+            make.height.equalTo(80)
+            make.trailing.equalToSuperview().offset(-15)
+            make.leading.equalToSuperview().offset(15)
+            make.centerX.equalTo(verticalStackView)
+        }
         
     }
     
