92dd247936266d49559f3379d18abf39ec4116b6
Zvontsov Sergey <zvoncov@gmail.com>
2023-02-02 11:33:53 +0300

    Add SnapKit

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -6,14 +6,96 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Variables
+    
+    var buttons = ButtonsProperty()
+    var labels = LabelProperty()
+    
+    //MARK: - UIButtons
+    
+    lazy var firstButton: UIButton = {
+        let button: UIButton = UIButton(frame: buttons.buttonFrame)
+        button.setBackgroundImage(buttons.firstButtonBackgroundImage, for: .normal)
+        button.setTitle("Choice one", for: .normal)
+        button.titleLabel?.textColor = .white
+        button.titleLabel?.font = buttons.buttonFont
+        button.addTarget(self, action: #selector(getAnswerForButtons), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var secondButton: UIButton = {
+        let button = UIButton(frame: buttons.buttonFrame)
+        button.setBackgroundImage(buttons.secondButtonBackgroundImage, for: .normal)
+        button.setTitle("Choice two", for: .normal)
+        button.titleLabel?.font = buttons.buttonFont
+        button.titleLabel?.textColor = .white
+        button.addTarget(self, action: #selector(getAnswerForButtons), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Labels
+    
+    lazy var questionLabel: UILabel = {
+        let label = UILabel(frame: labels.labelFrame)
+        label.font = labels.labelFont
+        label.textColor = labels.labelTextColor
+        label.numberOfLines = 0
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.text = "Question text."
+        return label
+    }()
+    
+    //MARK: - UIView and UIStackView
+    
+    lazy var verticalStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10.0
+        stack.alignment = .fill
+        stack.contentMode = .scaleToFill
+        stack.distribution = .fillProportionally
+        [   questionLabel,
+            firstButton,
+            secondButton].forEach {
+            stack.addArrangedSubview($0)
+        }
+        
+        return stack
+    }()
+    
+    lazy var backgroundImageView: UIImageView = {
+        let image = UIImageView(image: #imageLiteral(resourceName: "background"))
+        return image
+    }()
+    
+    //MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
+    
+    //MARK: - Set constraints
 
-
+    func setupUI() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(verticalStackView)
+        
+        
+        
+    }
+    
+    //MARK: - Methods
+    
+    @objc func getAnswerForButtons(_ sender: UIButton) {
+        
+    }
+    
 }
 
---
+++ Destini/Services/ButtonsProperty.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsProperty.swift
+//  Destini
+//
+//  Created by Sergey on 01.02.2023.
+//
+
+import Foundation
+import UIKit
+
+struct ButtonsProperty {
+    let buttonFrame: CGRect = CGRect(x: 0, y: 0, width: 374, height: 80)
+    let buttonFont: UIFont = UIFont.systemFont(ofSize: 25.0, weight: .regular)
+    let firstButtonBackgroundImage: UIImage = #imageLiteral(resourceName: "choice1Background")
+    let secondButtonBackgroundImage: UIImage = #imageLiteral(resourceName: "choice2Background")
+}
+
